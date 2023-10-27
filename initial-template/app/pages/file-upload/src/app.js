202091c6490a8423b8d2514ccb4e9208575f6a08
AngeloBordin <angelombordin@gmail.com>
2023-10-26 21:10:47 -0300

    create fake file fetch and reorganize file content

--- initial-template/app/pages/file-upload/src/app.js
+++ initial-template/app/pages/file-upload/src/app.js
@@ -1,48 +1,61 @@
-import Clock from './deps/clock.js';
+import Clock from "./deps/clock.js";
+import View from "./view.js";
 
-const fileUpload = document.getElementById('fileUpload')
-const btnUploadVideo = document.getElementById('btnUploadVideos')
-const fileSize = document.getElementById('fileSize')
-const fileInfo = document.getElementById('fileInfo')
-const txtfileName = document.getElementById('fileName')
-const fileUploadWrapper = document.getElementById('fileUploadWrapper')
-const elapsed = document.getElementById('elapsed')
+const worker = new Worker("./src/worker/worker.js", {
+	type: "module",
+});
 
+const view = new View();
+const clock = new Clock();
 
-fileUpload.addEventListener('change', onChange)
-btnUploadVideo.addEventListener('click', () => {
-    // trigger file input
-    fileUpload.click()
-})
-let took = ''
+worker.onmessage = ({ data }) => {
+	if (data.status !== "done") return;
+	clock.stop();
+	view.updateElapsedTime(`Process took ${took.replace("ago", "")}`);
+};
 
-function parseBytesIntoMBAndGB(bytes) {
-    const mb = bytes / (1024 * 1024)
-    // if mb is greater than 1024, then convert to GB
-    if (mb > 1024) {
-        // rount to 2 decimal places
-        return `${Math.round(mb / 1024)}GB`
-    }
-    return `${Math.round(mb)}MB`
+let took = "";
+view.configureOnFileChange((file) => {
+	const canvas = view.getCanvas();
+	worker.postMessage(
+		{
+			file,
+			canvas,
+		},
+		[canvas]
+	);
+
+	clock.start((time) => {
+		took = time;
+		view.updateElapsedTime(`Process started ${time}`);
+	});
+});
+
+// Função ṕara simular selecao do arquivo
+async function fakeFetch() {
+	const filePath = "/videos/frag_bunny.mp4";
+	const response = await fetch(filePath);
+
+	// Traz o tamanho do arquivo!
+	//   const response = await fetch(filePath, {
+	//     method: "HEAD",
+	//   });
+	// response.headers.get('content-length');
+	// debugger
+
+	// Simula o processo de geração do arquivo após selecionado no html
+	const file = new File([await response.blob()], filePath, {
+		type: "video/mp4",
+		lastModified: Date.now(),
+	});
+
+	// Cria o evento de change no input
+	const event = new Event("change");
+	// Cria a propriedade target dentro do evento e seta o valor da propriedade com o arquivo
+	Reflect.defineProperty(event, "target", { value: { files: [file] } });
+
+	// Dispara o evento criado com o arquivo já carregado dentro da div de upload
+	document.getElementById("fileUpload").dispatchEvent(event);
 }
-const clock = new Clock()
 
-function onChange(e) {
-    const file = e.target.files[0]
-    const { name, size } = file
-    txtfileName.innerText = name
-    fileSize.innerText = parseBytesIntoMBAndGB(size)
-
-    fileInfo.classList.remove('hide')
-    fileUploadWrapper.classList.add('hide')
-
-    clock.start((time) => {
-        took = time;
-        elapsed.innerText = `Process started ${time}`
-    })
-
-    setTimeout(() => {
-        clock.stop()
-        elapsed.innerText = `Process took ${took.replace('ago', '')}`
-    }, 5000)
-}
+fakeFetch();
